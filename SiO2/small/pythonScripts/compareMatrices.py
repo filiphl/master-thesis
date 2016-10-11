b6c2636f9b6426a05370a5c356a7fc9d6f065ccb
filiphl <filiphenriklarsen@gmail.com>
2016-10-11 19:53:21 +0200

    Weird behavior in force distribution. Trouble shooting

--- SiO2/small/pythonScripts/compareMatrices.py
+++ SiO2/small/pythonScripts/compareMatrices.py
@@ -18,16 +18,16 @@ class forces:
         for line in xrange(len(self.content)):
             self.content[line] = re.sub(r'\s*\d+\s\d*\.?\d*\s\d*\.?\d*\s\d+\s0\s0\s0\s?', r'', self.content[line])
 
-        self.content = filter(None, self.content)
+        self.content = filter(None, self.content) #Remove blank gaps
 
         self.nChunks = int(self.content[0].split()[1])
         self.snChunks = int(np.sqrt(self.nChunks))
 
         self.absoluteForces = np.zeros([self.snChunks, self.snChunks])
         self.matrix         = np.zeros([self.snChunks, self.snChunks, 3])
-        self.count          = np.zeros([self.snChunks, self.snChunks])
+        #self.count          = np.zeros([self.snChunks, self.snChunks])
 
-        self.binWidth = 4.01
+        self.binWidth = 7.121
 
         self.loadMatrix()
 
@@ -45,60 +45,90 @@ class forces:
                 fz = float(col[6])
                 self.absoluteForces[x,y] += np.sqrt(fx**2 + fy**2 + fz**2)
             else:
+                self.name='time step %03d'%steps
+                #plt.figure()
+                singlePlot = Plotter(self)
+                singlePlot.plotMatrix(save=False)
+                self.absoluteForces = np.zeros([self.snChunks, self.snChunks])
                 steps += 1
 
-        self.matrix /= steps
-        self.absoluteForces /= steps
-        #print "Steps = ", steps
+        #self.matrix /= steps
+        #self.absoluteForces /= steps
 
 class Plotter:
 
     def __init__(self, objects):
-
         if type(objects) == list:
             self.name = 'Averaged'
             self.snChunks = objects[0].snChunks
             self.absoluteForces = np.zeros([self.snChunks, self.snChunks])
-            self.plotAverage(objects)
+            self.average(objects)
 
         else:
             self.name = objects.name
             self.snChunks = objects.snChunks
             self.absoluteForces = objects.absoluteForces
-            #self.plotMatrix()
 
-    def plotAverage(self, objects):
+
+    def average(self, objects, save=False):
         for forceObject in objects:
             self.absoluteForces += forceObject.absoluteForces
         self.absoluteForces /= len(objects)
+        self.plotMatrix(save)
 
-    def plotEach(self, objects):
+    def plotEach(self, objects, save=False):
         for forceObject in objects:
             self.absoluteForces = forceObject.absoluteForces
             self.name = forceObject.name
+            plt.figure()
+            self.plotMatrix(save)
+
 
     def fmt(self, x, pos):
         a, b = '{:.2e}'.format(x).split('e')
         b = int(b)
         return r'${} \times 10^{{{}}}$'.format(a, b)
 
-    def plotMatrix(self):
+    def plotMatrix(self, save=False):
         plt.imshow(self.absoluteForces, interpolation='nearest', cmap="hot_r", )
-        cb = plt.colorbar(format=ticker.FuncFormatter(self.fmt))
+        plt.colorbar(format=ticker.FuncFormatter(self.fmt))
+        plt.clim(0,4)
+        #cb = plt.colorbar(format=ticker.FuncFormatter(self.fmt))
         #cb.ax.invert_yaxis()
         plt.title(self.name)
-        plt.show()
+        if save:
+            name = "%s"%"".join(self.name.split())
+            plt.savefig(name+".png", format="png")
+            plt.close()
+            print "Saved ", name
+        else:
+            plt.draw()
+            plt.clf()
 
-    def printMatrix(self):
-        print self.absoluteForces
 
 
+plt.figure()
+plt.ion()
+
+singleObject = forces('forceFiles/forcesAll.txt', 'all')
+#singlePlot = Plotter(singleObject)
 
 
+"""
+frame = 0
+for files in listdir('forceFiles/'):
+    singleObject = forces('forceFiles/'+files, 'Single frame t='+str(frame))
+    singlePlot = Plotter(singleObject)
+    singlePlot.plotMatrix()
+    frame += 100
 
 
+myObjects = []
+for files in listdir('forceFiles/'):
+    myObjects.append(forces('forceFiles/'+files, 'Averaged'))
+averagePlot = Plotter(myObjects)
+averagePlot.plotMatrix()
+"""
 
-singleObject = forces('forces.txt', 'average')
-singlePlot = Plotter(singleObject)
-singlePlot.plotMatrix()
-singlePlot.printMatrix()
+from scitools.std import *
+movie('timestep*.png', encoder='convert', fps=25, output_file='animation.gif')	# Make a gif
